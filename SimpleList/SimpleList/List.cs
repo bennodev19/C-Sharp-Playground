83d2965652d60d23f2fbc72813f84e77312de380
BennoDev <bennokohrs@gmail.com>
2021-10-01 12:29:03 +0200

    init

---
+++ SimpleList/SimpleList/List.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SimpleList
+{
+    public class List
+    {
+        public List()
+        {
+        }
+    }
+}
---
+++ SimpleList/SimpleList/Pupil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
