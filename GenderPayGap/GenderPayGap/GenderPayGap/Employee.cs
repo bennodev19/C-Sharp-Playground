f53f32180620c6d2c16ebf6c0c156461a9b034ad
BennoDev <bennokohrs@gmail.com>
2021-10-22 12:33:02 +0200

    x

---
+++ GenderPayGap/GenderPayGap/GenderPayGap/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace GenderPayGap
+{
+    public class Employee
+    {
+        private string id;
+
+        private string _name;
+        // https://stackoverflow.com/questions/6709072/c-getter-setter
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private float _salary;
+        public float salary
+        {
+            get { return _salary; }
+            set {
+                 _salary = this.formatSalary(value);
+            }
+        }
+
+        public Employee(string name, float salary)
+        {
+            id = Guid.NewGuid().ToString("N");
+            this.salary = this.formatSalary(salary);
+            this.name = name;
+        }
+
+        private float formatSalary(float value)
+        {
+            if (value >= 1)
+                return value;
+            else
+                return 0;
+        }
+    }
+}
---
+++ GenderPayGap/GenderPayGap/GenderPayGap/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GenderPayGap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee firstEmployee = new Employee("frank", -10);
+            printEmplyee(firstEmployee);
+
+            firstEmployee.salary = 89;
+            printEmplyee(firstEmployee);
+
+            Employee secondEmplyee = new Employee("jeff", 100);
+            printEmplyee(secondEmplyee);
+
+            secondEmplyee.salary = -100;
+            printEmplyee(secondEmplyee);          
+        }
+
+        static void printEmplyee(Employee emplyee)
+        {
+            Console.WriteLine("First Employee: (Name: '" + emplyee.name + "', Salary: " + emplyee.salary + ")");
+        }
+    }
+}
