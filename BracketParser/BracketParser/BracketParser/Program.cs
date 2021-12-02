face55169136bc36837d7a8f1b0b07663c0a40f0
BennoDev <bennokohrs@gmail.com>
2021-12-02 11:31:31 +0100

    program

---
+++ BracketParser/BracketParser/BracketParser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BracketParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string toParseValue = "{}";
+            BracketParser bracketParser = new BracketParser(toParseValue);
+            Console.WriteLine("Stripped value: '" + bracketParser.getStrippedValue() + "'");
+            
+            Console.WriteLine("Is parsed value valid: " + bracketParser.parse());
+        }
+    }
+}
